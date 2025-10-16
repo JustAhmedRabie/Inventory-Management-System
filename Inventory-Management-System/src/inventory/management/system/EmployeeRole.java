6e1f54e66e27f7b87e8ada730ab6c9b793baf2fe
ahmed_rabie_eg <eizomizo12@gmail.com>
2025-10-17 02:37:50 +0300

    Created EmployeeRole class

---
+++ Inventory-Management-System/src/inventory/management/system/EmployeeRole.java
@@ -0,0 +1,22 @@
+﻿package inventory.management.system;
+
+public class EmployeeRole extends UserRole
+{
+    private ProductDatabase productDatabase;
+    private CustomerProductDatabase customerProductDatabase;
+
+    public EmployeeRole()
+    {
+        productDatabase = new ProductDatabase("Products.txt");
+        customerProductDatabase = new CustomerProductDatabase("CustomersProducts.txt");
+        productDatabase.readFromFile();
+        customerProductDatabase.readFromFile();
+    }
+
+    @Override
+    public void logout()
+    {
+        productDatabase.saveToFile();
+        customerProductDatabase.saveToFile();
+    }
+}
