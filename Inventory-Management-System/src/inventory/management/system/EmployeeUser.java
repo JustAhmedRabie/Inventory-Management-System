848b88edce4fa6b53533868eeb4341ab903e262d
Abdelrahman Ibrahim Mohamed Ragab <es.abdelrahman.kotb2023@alexu.edu.eg>
2025-10-16 20:55:05 +0300

    Added First class EmployeeUser Also added class methods

---
+++ Inventory-Management-System/src/inventory/management/system/EmployeeUser.java
@@ -0,0 +1,34 @@
+
+package inventory.management.system;
+
+
+public class EmployeeUser extends Record {
+    //employeeId, Name, Email, Address, PhoneNumber 
+    private String employeeId;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+
+    public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+
+    @Override
+    public String lineRepresentation() {
+        return employeeId + "," + name + "," + email + "," + address + "," + phoneNumber;
+    }
+
+    @Override
+    public String getSearchKey() {
+        return employeeId; 
+    }
+    
+    
+}
