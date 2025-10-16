87bf249cc07b3613e1fdd10273584b5d5224c54e
Abdelrahman Ibrahim Mohamed Ragab <es.abdelrahman.kotb2023@alexu.edu.eg>
2025-10-17 02:15:26 +0300

    Added AdminRole Class (done)

---
+++ Inventory-Management-System/src/inventory/management/system/AdminRole.java
@@ -0,0 +1,45 @@
+
+package inventory.management.system;
+
+import java.util.ArrayList;
+
+
+public class AdminRole extends UserRole {
+    
+    private EmployeeUserDatabase database;
+    
+    public AdminRole(){
+        database = new EmployeeUserDatabase("Employees.txt");
+        database.readFromFile();
+    }
+    
+    
+     public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
+         database.insertRecord(new EmployeeUser(employeeId,name,email,address,phoneNumber));
+     }
+    
+     
+     public EmployeeUser[] getListOfEmployees(){
+         ArrayList<Record> r = database.returnAllRecords();
+         int n = r.size();
+         EmployeeUser[] arr = new EmployeeUser[n];
+         for(int i=0;i<n;i++){
+             arr[i] = (EmployeeUser) r.get(i);
+         }
+         return arr;
+     }
+    
+     
+     public void removeEmployee(String key){
+         database.deleteRecord(key);
+     }
+     
+
+    @Override
+    public void logout() {
+        database.saveToFile();
+    }
+    
+    
+    
+}
